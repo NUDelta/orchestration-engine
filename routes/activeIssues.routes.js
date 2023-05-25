2833872582aa60ce2e7c56022a526aefd02b0aec
Kapil Garg <kapil1garg@gmail.com>
2023-05-25 23:08:08 +0000

    routes to fetch active issues

--- routes/activeIssues.routes.js
+++ routes/activeIssues.routes.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import { createActiveIssue } from '../controllers/modelControllers/activeIssuesController.js';
 import { getRefreshedObjsForTarget } from '../controllers/execution/executionFns.js';
 import { computeStrategies } from '../controllers/execution/executionFlow.js';
+import { ActiveIssues } from '../models/activeIssues.js';
+import { getFromStudioAPI } from '../imports/studioAPI/requests.js';
 
 export const activeIssuesRouter = new Router();
 
@@ -75,9 +77,80 @@ activeIssuesRouter.post('/createActiveIssue', async (req, res) => {
   } catch (error) {
     let errorMessage = `Error when creating ActiveIssue via API route: ${error.stack}`;
     console.error(errorMessage);
-    res.send(errorMessage);
+    res.json(errorMessage);
   }
 });
 
 // TODO: implement
 activeIssuesRouter.post('/updateActiveIssue', async (req, res) => {});
+
+/**
+ * Returns all active issues.
+ * request body: {}
+ * TODO: probably should have 1 fetch function with query params
+ */
+activeIssuesRouter.get('/fetchActiveIssues', async (req, res) => {
+  try {
+    const activeIssues = await ActiveIssues.find({});
+    res.status(200).json(activeIssues);
+  } catch (error) {
+    let errorMessage = `Error when fetching all ActiveIssues via API route: ${error.stack}`;
+    console.error(errorMessage);
+    res.json(errorMessage);
+  }
+});
+
+/**
+ * Returns all active issues for a given project.
+ * request query params: ?projectName=string
+ * TODO: should allow filtering by venue
+ */
+activeIssuesRouter.get('/fetchActiveIssuesForProject', async (req, res) => {
+  try {
+    // get project name from query
+    const { projectName } = req.query;
+
+    // fetch all active issues for the project
+    const activeIssues = await ActiveIssues.find({
+      'issue_target.name': projectName,
+    });
+    res.status(200).json(activeIssues);
+  } catch (error) {
+    let errorMessage = `Error when fetching all ActiveIssues via API route: ${error.stack}`;
+    console.error(errorMessage);
+    res.json(errorMessage);
+  }
+});
+
+/**
+ * Returns all active issues for a given SIG.
+ * request query params: ?sigName=string
+ * TODO: should allow filtering by venue
+ */
+activeIssuesRouter.get('/fetchActiveIssuesForSig', async (req, res) => {
+  try {
+    // get SIG name from query
+    const { sigName } = req.query;
+
+    // get all projects for the SIG from the Studio API
+    const response = await getFromStudioAPI('projects', {
+      populateTools: false,
+    });
+    const projectsForSig = response.body.filter((project) => {
+      return project.sig_name === sigName;
+    });
+    const projectNames = projectsForSig.map((project) => project.name);
+
+    // fetch all active issues for the list of projects
+    const activeIssues = await ActiveIssues.find({
+      'issue_target.name': {
+        $in: projectNames,
+      },
+    });
+    res.status(200).json(activeIssues);
+  } catch (error) {
+    let errorMessage = `Error when fetching all ActiveIssues via API route: ${error.stack}`;
+    console.error(errorMessage);
+    res.json(errorMessage);
+  }
+});
